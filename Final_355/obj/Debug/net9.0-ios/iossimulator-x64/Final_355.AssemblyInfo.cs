cd60f27010a99f2279a047ed20f5abac9d9fff8d
FarVillage117 <143549383+FarVillage117@users.noreply.github.com>
2025-04-29 11:32:06 -0400

    Minor Change

--- Final_355/obj/Debug/net9.0-ios/iossimulator-x64/Final_355.AssemblyInfo.cs
+++ Final_355/obj/Debug/net9.0-ios/iossimulator-x64/Final_355.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_355")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a6ea4f20cffe99f3dacc7670ee1988b8cc051c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_355")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final_355")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
