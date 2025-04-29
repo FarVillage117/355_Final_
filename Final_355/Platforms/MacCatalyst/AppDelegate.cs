a6ea4f20cffe99f3dacc7670ee1988b8cc051c31
FarVillage117 <143549383+FarVillage117@users.noreply.github.com>
2025-04-29 11:24:26 -0400

    Initial File Make

---
+++ Final_355/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Final_355
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
